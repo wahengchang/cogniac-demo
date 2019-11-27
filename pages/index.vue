0dec7addf6282aa1e51f92d72cd2b339009985c8
peter chang <wahengchang@gmail.com>
2019-11-27 23:12:28 +0800

    remove user name - demo

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
       <p v-if="formError" class="error">
         {{ formError }}
       </p>
-      <p><i>To login, use <b>demo</b> as username and <b>demo</b> as password.</i></p>
+      <p><i>To login.</i></p>
       <p>Username: <input v-model="formUsername" type="text" name="username"></p>
       <p>Password: <input v-model="formPassword" type="password" name="password"></p>
       <button type="submit">
